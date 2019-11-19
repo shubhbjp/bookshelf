f743e71859895b2cd9e1266fe29c458c65fac7a0
bajpai <shubham.bajpai@railyatri.in>
2019-11-19 13:41:52 +0530

    Search changes from db

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,24 +1,46 @@
 require 'redis'
 class SearchController < ApplicationController
 
-	def search_list	  
-	  if params['key'].nil? || params['key'].empty?
-	  	return
+	def search_list
+    @data = []
+    if params['key'].nil? || params['key'].empty?
+      return
 	  else
 		redis = Redis.new(host: "localhost")
 		key_exists = redis.exists(params['key'])
-		if key_exists
-			data = JSON.parse(redis.get(params['key']))
-		else
-			book_data_from_db = Book.where(title: params['key'])
-			data = []
-			book_data_from_db.each {
-				|book_data|
-				data.push({'name' => book_data.title, 'release_date' => book_data.release_date, 'writer' => Writer.find(book_data.writers_id).name}) 
-			}
-			redis.set(params['key'], data.to_json)
-		end
+		  if key_exists
+			 @data = JSON.parse(redis.get(params['key']))
+		  else  
+        book_data_from_db = Book.where('title': params['key'])
+				book_data_from_db.each { 
+  				|book| 
+          authors_data = []
+          book.writers_id.each {
+              |writer_id| 
+                writer_data = Writer.find(writer_id)
+                temp_writer_data = Hash.new(0)
+                temp_writer_data['name'] = writer_data.name
+                temp_writer_data['email'] = writer_data.email
+                temp_writer_data['mobile'] = writer_data.mobile
+                temp_writer_data['address'] = writer_data.address + ", " + writer_data.city['name'] + ", " + writer_data.state['name'] + ", " + writer_data.country['name'] + ". Pincode: " + writer_data.pincode.to_s
+                authors_data.push(temp_writer_data)
+            }
+            publication_data = Publication.find(book.publications_id)
+            book_data_in_hash = Hash.new(0)
+            book_data_in_hash['title'] = book.title
+            book_data_in_hash['about'] = book.about
+            book_data_in_hash['category'] = book.category['name']
+            book_data_in_hash['publication_date'] = book.publication_date
+            book_data_in_hash['writers_info'] = authors_data
+            book_data_in_hash['publishers_info'] = Hash.new(0)
+            book_data_in_hash['publishers_info']['name'] = publication_data.name
+            book_data_in_hash['publishers_info']['email'] = publication_data.email
+            book_data_in_hash['publishers_info']['contact'] = publication_data.contact
+            book_data_in_hash['publishers_info']['address'] = publication_data.address 
+            @data.push(book_data_in_hash)
+			   }
+			redis.set(params['key'], @data.to_json)
+		  end
 	  end
-	  #Search.save_data
 	end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,5 @@
 class Search
-  
-  def self.save_data
-  end
+  has_many :books
+  has_and_belongs_to_many :writers
+  belongs_to :publications
 end
